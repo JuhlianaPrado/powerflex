d04914638082203f57666ea2d139ff5a761813b2
Juliana Nunes do Prado <rm93527@estudante.fieb.edu.br>
2024-07-29 20:33:26 -0300

    Início ambiente de desenvolvimento

---
+++ src/main/java/com/itb/mif3an/academia/AcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/mif3an/academia/AcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
